b544be2bd0614363ba1f3bae53ab11d0bcba2841
souldb20 <souldb20@gmail.com>
2021-06-12 14:21:17 +0900

    구구단게임

---
+++ README.md
@@ -0,0 +1,3 @@
+# GuGuDan WebGame 
+
+리액트로 만든 구구단 웹게임
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    name: 'gugudan-setting',
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.jsx', '.js'],
+    },
+
+    entry: {
+        app: ['./client'],
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 5% in KR'],
+                        },
+                    }],
+                    ['@babel/preset-react']
+                ],
+                plugins: ['@babel/plugin-proposal-class-properties'],
+            }
+        }],
+    },
+    output: {
+        filename: 'app.js',
+        path: path.join(__dirname, 'dist'),
+    },
+};
